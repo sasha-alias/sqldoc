85fed4128614ac5b67aca55044f947f9dd709dc9
Sasha Aliashkevich <olsender@gmail.com>
2015-08-13 18:06:12 +0200

    fix several blocks messup

--- test/test.js
+++ test/test.js
@@ -40,7 +40,13 @@ describe('SqlDoc:', function(){
     it('check chart', function(){
         var data = [{"query":"--- chart pie\nSELECT city, COUNT(*) FROM employees GROUP BY city\n\n","datasets":[{"nrecords":3,"fields":[{"name":"city","type":25},{"name":"count","type":20}],"data":[["London","245"],["Paris","153"],["New York","323"]],"cmdStatus":"SELECT 3","resultStatus":"PGRES_TUPLES_OK","resultErrorMessage":""}],"start_time":2864.9700000023586,"duration":50.404}]
         var doc = React.createElement(SqlDoc, {data:data});
-        console.log(React.renderToStaticMarkup(doc));
+        //console.log(React.renderToStaticMarkup(doc));
 
+    });
+
+    it('multiple sql blocks', function(){
+        var data = [{"query":"--- chart pie\nSELECT city, COUNT(*) FROM employees GROUP BY city\n","datasets":[{"nrecords":3,"fields":[{"name":"city","type":25},{"name":"count","type":20}],"data":[["London","245"],["Paris","153"],["New York","323"]],"cmdStatus":"SELECT 3","resultStatus":"PGRES_TUPLES_OK","resultErrorMessage":""}],"start_time":11782.757000000856,"duration":52.473},{"query":"--- chart line\nselect * from generate_series(1, 10)\n","datasets":[{"nrecords":10,"fields":[{"name":"generate_series","type":23}],"data":[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["10"]],"cmdStatus":"SELECT 10","resultStatus":"PGRES_TUPLES_OK","resultErrorMessage":""}],"start_time":11836.935999999696,"duration":23.839}]
+        var doc = React.createElement(SqlDoc, {data:data});
+        console.log(React.renderToStaticMarkup(doc));
     });
 });
